753fa0e086e7fe8670dd37c5c5e10a8e6d373cec
Ali Shayesteh <ali.shayesteh@gmail.com>
2023-01-06 13:21:44 +0330

    hold all pictures

--- src/hooks/usePics.js
+++ src/hooks/usePics.js
@@ -44,9 +44,7 @@ const usePics = () => {
   };
 
   useEffect(() => {
-    setHold([
-      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    ]);
+    setHold(pics.map(pic => pic.id));
     setTimeout(() => {
       setHold([]);
     }, 2000);
